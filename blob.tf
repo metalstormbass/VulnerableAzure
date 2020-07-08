c71c20359a10f0efcad5ef5e855f0f117234c94b
Michael Braun <michaelbraunbass@gmail.com>
2020-07-08 13:58:28 -0600

    Update blob.tf

--- blob.tf
+++ blob.tf
@@ -35,7 +35,7 @@ resource "azurerm_storage_account" "victim-private-storage" {
    network_rules {
     default_action             = "allow"
     ip_rules                   = ["$var.internal-private-ip"]
-    virtual_network_subnet_ids = azurerm_subnet.victim-network-subnet.id
+    virtual_network_subnet_ids = ["azurerm_subnet.victim-network-subnet.id"]
   }
 
 
