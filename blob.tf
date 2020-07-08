58e6dc7c88c599e026c655dd0bd05c75a800f46a
Michael Braun <michaelbraunbass@gmail.com>
2020-07-08 12:45:27 -0600

    Update blob.tf

--- blob.tf
+++ blob.tf
@@ -25,33 +25,33 @@ resource "azurerm_storage_blob" "victim-public-blob" {
 
 #################################Backend Private Blob
 
-resource "azurerm_storage_account" "victim-public-storage" {
+resource "azurerm_storage_account" "victim-private-storage" {
   name                     = "victimprivatestorage"
   resource_group_name      = azurerm_resource_group.victim-network-rg.name
   location                 = azurerm_resource_group.victim-network-rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-}
-
-resource "azurerm_storage_container" "victim-private-container" {
-  name                  = "${var.vulnvm-name}-backend"
-  storage_account_name  = azurerm_storage_account.victim-public-storage.name
-  container_access_type = "private"
-}
-
-resource "azurerm_storage_blob" "victim-private-blob" {
-  name                   = "secrets.txt"
-  storage_account_name   = azurerm_storage_account.victim-public-storage.name
-  storage_container_name = azurerm_storage_container.victim-public-container.name
-  type                   = "Block"
-# source                 = "files/personalinformation.zip"
- 
- network_rules {
+  
+   network_rules {
     default_action             = "allow"
     ip_rules                   = var.internal-private-ip
     virtual_network_subnet_ids = victim-network-subnet.name
   }
 
 
+}
+
+resource "azurerm_storage_container" "victim-private-container" {
+  name                  = "${var.vulnvm-name}-backend"
+  storage_account_name  = azurerm_storage_account.victim-private-storage.name
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_blob" "victim-private-blob" {
+  name                   = "secrets.txt"
+  storage_account_name   = azurerm_storage_account.victim-private-storage.name
+  storage_container_name = azurerm_storage_container.victim-private-container.name
+  type                   = "Block"
+# source                 = "files/personalinformation.zip"
 
 }
