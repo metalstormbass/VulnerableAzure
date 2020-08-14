09a1d4c0102434837df9e7f53162a4f61f587ab8
Michael Braun <michaelbraunbass@gmail.com>
2020-08-14 12:21:38 -0600

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,11 @@
 # Output the public ip of the gateway
 
-output "Vulnerable_VM_IP" {
-    value = azurerm_public_ip.vulnpublicip.ip_address
-}
-
-
 output "Company_Name" {
     value = var.victim_company
+}
+
+output "Vulnerable_VM_IP" {
+    value = azurerm_public_ip.vulnpublicip.ip_address
 }
 
 output "k8_service_url" {
