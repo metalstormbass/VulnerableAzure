34dee79c4b09414d1c5bdac7da6767a53da46033
Michael Braun <michaelbraunbass@gmail.com>
2020-08-14 10:41:07 -0600

    Update aks.tf

--- aks.tf
+++ aks.tf
@@ -33,9 +33,9 @@ resource "azurerm_kubernetes_cluster" "vuln_k8_cluster" {
 #Provider for K8, used after built
 provider "kubernetes" {
     host                   = azurerm_kubernetes_cluster.vuln_k8_cluster.kube_config.0.host
-    client_certificate     = base64decode(azurerm_kubernetes_cluster.vuln_k8_cluster.kube_config.0.client_certificate)
-    client_key             = base64decode(azurerm_kubernetes_cluster.vuln_k8_cluster.kube_config.0.client_key)}
-    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.vuln_k8_cluster.kube_config.0.cluster_ca_certificate)
+    client_certificate     = "${base64decode(azurerm_kubernetes_cluster.vuln_k8_cluster.kube_config.0.client_certificate)}"
+    client_key             = "${base64decode(azurerm_kubernetes_cluster.vuln_k8_cluster.kube_config.0.client_key)}"
+    cluster_ca_certificate = "${base64decode(azurerm_kubernetes_cluster.vuln_k8_cluster.kube_config.0.cluster_ca_certificate)}"
 }
 
 
