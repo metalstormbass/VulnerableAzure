d22478ea77aa7bbc6f2a0239be0ad179114c6661
Michael Braun <michaelbraunbass@gmail.com>
2020-08-19 12:47:56 -0600

    Update app_service.tf

--- app_service.tf
+++ app_service.tf
@@ -1,3 +1,4 @@
+#Create App Service Plan
 resource "azurerm_app_service_plan" "vulnerablewebapp_serviceplan" {
   name                = "${var.victim_company}-app"
   location            = azurerm_resource_group.victim-network-rg.location
@@ -9,6 +10,7 @@ resource "azurerm_app_service_plan" "vulnerablewebapp_serviceplan" {
   }
 }
 
+#Create App Service
 resource "azurerm_app_service" "vulnerablewebapp_appservice" {
   name                = "${var.victim_company}-app-service"
   location            = azurerm_resource_group.victim-network-rg.location
@@ -19,13 +21,13 @@ resource "azurerm_app_service" "vulnerablewebapp_appservice" {
   site_config {
     always_on                 = "true"
     dotnet_framework_version = "v4.0"
-    python_version           = "3.7"
-    scm_type                 = "GitHub"
-  }
-  
-  source_control{
-  branch = "master"
-  repo_url = "https://github.com/metalstormbass/VulnerableWebApp"
-  }
+    python_version           = "3.4"
+      }
+}
 
+resource "azurerm_app_service_source_control" "example" {
+  app_service_id        = azurerm_app_service.vulnerablewebapp_appservice.id
+  repo_url              = "https://github.com/metalstormbass/VulnerableWebApp.git"
+  is_manual_integration = true
+  branch                = "master"
 }
