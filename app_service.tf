cc66bbbc34a1118a5ac164d8baa5375ace63adfc
metalstormbass <michaelbraunbass@gmail.com>
2020-08-19 19:36:45 -0600

    modified:   app_service.tf

--- app_service.tf
+++ app_service.tf
@@ -30,7 +30,7 @@ resource "azurerm_app_service" "vulnerablewebapp_appservice" {
 #Provision the web app (https://github.com/terraform-providers/terraform-provider-azurerm/issues/1104)
 resource "null_resource" "scm_integration" {
   provisioner "local-exec" {
-    command     = "powershell/enable_scm.ps1 -webAppName ${azurerm_app_service.vulnerablewebapp_appservice.name} -appResourceGroupName ${azurerm_resource_group.victim-network-rg.name} -scmBranch master -repoUrl https://github.com/metalstormbass/VulnerableWebApp.git
+    command     = "powershell/enable_scm.ps1 -webAppName ${azurerm_app_service.vulnerablewebapp_appservice.name} -appResourceGroupName ${azurerm_resource_group.victim-network-rg.name} -scmBranch master -repoUrl https://github.com/metalstormbass/VulnerableWebApp.git"
     interpreter = ["pwsh", "-Command"]
   }
 }
