9e626fc73811eedc64cd1c8466a4fd6bfab2515b
metalstormbass <michaelbraunbass@gmail.com>
2020-08-19 20:13:00 -0600

    Yaya!

--- app_service.tf
+++ app_service.tf
@@ -4,6 +4,7 @@ resource "azurerm_app_service_plan" "vulnerablewebapp_serviceplan" {
   location            = azurerm_resource_group.victim-network-rg.location
   resource_group_name = azurerm_resource_group.victim-network-rg.name
   kind = "Linux"
+  reserved = true
   sku {
     tier = "Standard"
     size = "S1"
