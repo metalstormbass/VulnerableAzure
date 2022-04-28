be8519d5a642f3a94294ec76a9bcae8d0e8dfebf
Michael Braun <michaelbraunbass@gmail.com>
2022-04-28 08:56:38 -0600

    Update aks.tf

--- aks.tf
+++ aks.tf
@@ -42,7 +42,7 @@ provider "kubernetes" {
     client_certificate     = base64decode(azurerm_kubernetes_cluster.vuln_k8_cluster.kube_config.0.client_certificate)
     client_key             = base64decode(azurerm_kubernetes_cluster.vuln_k8_cluster.kube_config.0.client_key)
     cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.vuln_k8_cluster.kube_config.0.cluster_ca_certificate)
-    load_config_file = false
+  
 }
 
 
